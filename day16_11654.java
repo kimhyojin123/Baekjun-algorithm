1464e1fb4eb6934450c33da09dee52527d66aa30
gywls9900 <gywls9900@naver.com>
2020-01-27 21:51:29 +0900

    day16(아스키코드)

---
+++ day16_11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day16_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		String a=scan.nextLine();
+		char b=a.charAt(0);
+		System.out.print((int)b);
+	}
+
+}
